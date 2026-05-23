aee1dce3c9490f8ecf643df947ac62a698ba4d29
Talha Raj <molabuxr@gmail.com>
2026-05-23 10:34:09 +0500

    removed creds

--- src/utils/Creds.js
+++ src/utils/Creds.js
@@ -1,4 +1,4 @@
 export const Access_Token_MapBox =
-  'pk.eyJ1IjoidGFsaGEtcmFqMSIsImEiOiJjbWpremxoZmowNTZiM2VwaXQybDBja3hrIn0.1DAr1Iep5U7-2wiTpvxqiQ';
+  'replace_secret_key';
 
-export const GOOGLE_MAPS_API_KEY = 'AIzaSyBjDbYawVtbkap69deuj9qdvsak8VCTeIo';
+export const GOOGLE_MAPS_API_KEY = 'replace_secret_key';
